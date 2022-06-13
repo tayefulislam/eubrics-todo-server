5e02119ccbd3d17d4dbca00624b599b1b0fef1e3
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-13 17:04:20 +0600

    get all behavoirs api created

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middle ware
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = process.env.uri;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+
+async function run() {
+
+    try {
+
+        await client.connect()
+
+        const behaviorsCollection = client.db("eubricsTodo").collection("behaviors");
+
+
+        app.get('/behaviors', async (req, res) => {
+            const result = await behaviorsCollection.find({}).toArray()
+            res.send(result);
+        })
+
+
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
+
+
+
 
 app.get('/', (req, res) => {
     res.send('server is runnig')
